a83874c18585fc05bb3c2b333be39607f353b111
sheltonjoseph <shelton013@gmail.com>
2025-01-18 17:33:37 +0530

    build error fixed

--- src/components/sections/Experience.tsx
+++ src/components/sections/Experience.tsx
@@ -1,60 +1,60 @@
-import React from "react";
+// import React from "react";
 import {
   VerticalTimeline,
-  VerticalTimelineElement,
+  // VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 
 import "react-vertical-timeline-component/style.min.css";
 
-import { experiences } from "../../constants";
+// import { experiences } from "../../constants";
 import { SectionWrapper } from "../../hoc";
 import { Header } from "../atoms/Header";
-import { TExperience } from "../../types";
+// import { TExperience } from "../../types";
 import { config } from "../../constants/config";
 
-const ExperienceCard: React.FC<TExperience> = (experience) => {
-  return (
-    <VerticalTimelineElement
-      contentStyle={{
-        background: "#1d1836",
-        color: "#fff",
-      }}
-      contentArrowStyle={{ borderRight: "7px solid  #232631" }}
-      date={experience.date}
-      iconStyle={{ background: experience.iconBg }}
-      icon={
-        <div className="flex h-full w-full items-center justify-center">
-          <img
-            src={experience.icon}
-            alt={experience.companyName}
-            className="h-[60%] w-[60%] object-contain"
-          />
-        </div>
-      }
-    >
-      <div>
-        <h3 className="text-[24px] font-bold text-white">{experience.title}</h3>
-        <p
-          className="text-secondary text-[16px] font-semibold"
-          style={{ margin: 0 }}
-        >
-          {experience.companyName}
-        </p>
-      </div>
+// const ExperienceCard: React.FC<TExperience> = (experience) => {
+//   return (
+//     <VerticalTimelineElement
+//       contentStyle={{
+//         background: "#1d1836",
+//         color: "#fff",
+//       }}
+//       contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+//       date={experience.date}
+//       iconStyle={{ background: experience.iconBg }}
+//       icon={
+//         <div className="flex h-full w-full items-center justify-center">
+//           <img
+//             src={experience.icon}
+//             alt={experience.companyName}
+//             className="h-[60%] w-[60%] object-contain"
+//           />
+//         </div>
+//       }
+//     >
+//       <div>
+//         <h3 className="text-[24px] font-bold text-white">{experience.title}</h3>
+//         <p
+//           className="text-secondary text-[16px] font-semibold"
+//           style={{ margin: 0 }}
+//         >
+//           {experience.companyName}
+//         </p>
+//       </div>
 
-      <ul className="ml-5 mt-5 list-disc space-y-2">
-        {experience.points.map((point, index) => (
-          <li
-            key={`experience-point-${index}`}
-            className="text-white-100 pl-1 text-[14px] tracking-wider"
-          >
-            {point}
-          </li>
-        ))}
-      </ul>
-    </VerticalTimelineElement>
-  );
-};
+//       <ul className="ml-5 mt-5 list-disc space-y-2">
+//         {experience.points.map((point, index) => (
+//           <li
+//             key={`experience-point-${index}`}
+//             className="text-white-100 pl-1 text-[14px] tracking-wider"
+//           >
+//             {point}
+//           </li>
+//         ))}
+//       </ul>
+//     </VerticalTimelineElement>
+//   );
+// };
 
 const Experience = () => {
   return (
@@ -63,9 +63,9 @@ const Experience = () => {
 
       <div className="mt-20 flex flex-col">
         <VerticalTimeline>
-          {experiences.map((experience, index) => (
+          {/* {experiences.map((experience, index) => (
             <ExperienceCard key={index} {...experience} />
-          ))}
+          ))} */}
         </VerticalTimeline>
       </div>
     </>
